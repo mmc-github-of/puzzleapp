8efdd8c0de23ef45db2ccf5fcf72c8770b30891c
Manmohan Chauhan <manchauh@microsoft.com>
2020-04-13 20:55:56 +0530

    Added calculation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'puzzleapp';
+  title = 'Puzzule App';
+
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
+
+  x1y1: number;
+  x2y2: number;
+  x1x2: number;
+  y1y2: number;
+
+  ngOnInit(): void {
+    this.x1x2 = this.x2y2 = this.x1y1 = this.y1y2 = 0;
+    this.x1 = this.x2 = this.y1 = this.y2 = 0;
+  }
+   
+  calculateData() {
+    this.y1 = (this.x1y1 + this.x2y2 + this.y1y2 - this.x1x2) / 2;
+    this.y2 = this.y1y2 - this.y1;
+    this.x1 = this.x1y1-this.y1;
+    this.x2 = this.x2y2 + this.y2;
+  }
 }
